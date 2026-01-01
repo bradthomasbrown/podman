d42e390f02f35698962f50d87432caed677027d2
Brad Brown <bradthomasbrown@outlook.com>
2026-01-01 03:57:17 -0600

    i don't recall error handling being so ass, but this seems necessary to be comprehensive

--- test.ts
+++ test.ts
@@ -1,10 +1,10 @@
-import { Client } from "@bradthomasbrown/jra";
+import { Client } from "../jra/src/jra";
 import { Podman } from "./src/podman";
 
 const id = await Podman.run([["detach"], ["publish", "8545:8545"]], "ethereum/client-go", ["--dev", "--http", "--http.addr", "0.0.0.0", "--http.api", "eth,debug"]);
-await new Promise(r => setTimeout(r, 1000));
+await new Promise(r => setTimeout(r, 100));
 const client = new Client("localhost:8545");
-const result = await client.request("eth_blockNumber", [], "eth_chainId", []).catch(e => e as AggregateError);
+const result = await client.request("eth_blockNumber", [], "eth_chainId", []).catch(e => e as Error|AggregateError);
 if (result instanceof AggregateError) {
     if (result.message !== undefined) {
         console.error(result.message);
@@ -15,6 +15,12 @@ if (result instanceof AggregateError) {
         console.dir({ cause:error.cause }, { depth:null });
     }
     await Podman.remove([["force"]], id);
+    process.exit(1);
+}
+if (result instanceof Error) {
+    await Podman.remove([["force"]], id);
+    console.error(result.message);
+    console.log(result.stack);
     process.exit(1);
 }
 const [blockNumber, chainId] = result;
